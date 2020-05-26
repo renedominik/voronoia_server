db1dc99b7741660c13ee64e0390560021ddb6fd8
hildilab <hildilab@proteinformatics.uni-leipzig.de>
2020-05-26 19:54:35 +0000

    first running version

--- V4RNA_local.py
+++ V4RNA_local.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, url_for, redirect, request
+from flask import Flask, render_template, session, url_for, redirect, request, send_from_directory, jsonify
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from flask_mail import Mail
@@ -10,9 +10,10 @@ from werkzeug import secure_filename
 from datetime import datetime
 import random, string, os
 import getpass
-from threading import Thread
+# from threading import Thread
+# import thread
 import time
-from subprocess import Popen, PIPE
+#from subprocess import Popen, PIPE
 import subprocess
 
 import sqlite3 as sql
@@ -24,13 +25,14 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['FLASKY_MAIL_SENDER'] = 'PROFESSIONAL TESTER <monodualist121212@gmail.com>'
+app.config['USER_DATA_DIR'] = "/disk/user_data/v4rna/sessions/"
 
 
 bootstrap = Bootstrap(app)
 
 mail = Mail(app)
 
-sql_db = "jobs.db"
+sql_db = "/disk/data/v4rna/jobs.db"
 
 
 class InputForm(FlaskForm):
@@ -47,6 +49,7 @@ class InputForm(FlaskForm):
         print("validate:",fn)
         return '.' in fn and (fn.rsplit('.', 1)[-1] == 'pdb')
 
+    
 def send_email(to, subject, template, **kwargs):
     print('hi')
     msg = Message(subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
@@ -61,116 +64,146 @@ def index():
     #send_email('monodualismus121212@gmail.com', 'hi', 'mail/test')
     return render_template('home.html')
 
+
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
     print('submit a job')
     form = InputForm()
-    if request.method == 'POST':
-        print('post')
-        #print( getpass.getuser())
-        #print(os.getcwd())
+    if request.method == 'GET':
+        # GET:
+        return render_template('formular.html', form=form, tag_str="tag")
 
-        #EINLESEN
-        f = request.files['pdb']
-        if f.filename == '':
-            f = request.files.get('dnd')
-        atomRadii = request.form.get('options')
-        tag = request.form['tag']
-        email = request.form['email']
+    print('post')
+
+    f = request.files['pdb']
+    if f.filename == '':
+        f = request.files.get('dnd')
+    atomRadii = request.form.get('options')
+    tag = request.form['tag']
+    email = request.form['email']
         
+    print(tag)
         
-        
-        
-        
-        
-        #Festlegen vom Pfad
-        #output_dir = '/disk/user_data/v4rna/sessions/'
-        output_dir = '/home/hstudent/Desktop/voronoia/webapp/data/'
-        if email != '':
-            output_dir += email + '/'
+    #Festlegen vom Pfad
+    output_dir = '/disk/user_data/v4rna/sessions/'
+    # output_dir = '/disk/user_data/v4rna/sessions/'
+    #output_dir = '/home/hstudent/Desktop/voronoia/webapp/data/'
+    if email != '' :
+        output_dir += email + '/'
+        if not os.path.exists( output_dir):
             os.mkdir(output_dir)
-        else:
-            output_dir += 'anonymous/'
-            if not os.path.isdir( output_dir):
-                os.mkdir(output_dir)
+    else:
+        output_dir += 'anonymous/'
+        if not os.path.isdir( output_dir):
+            os.mkdir(output_dir)
                 
-        #Zusammenfügen des Directorynamens
-        now = datetime.now()
-        date = now.strftime("%Y%m%d%H%M")
-        random_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
-        id_str = date + '_' + random_id +'/'
-        output_dir += id_str
-        print( output_dir)
-        os.mkdir( output_dir)
+    #Zusammenfuegen des Directorynamens
+    now = datetime.now()
+    date = now.strftime("%Y%m%d%H%M")
+    # random_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    #id_str = date + '_' + random_id +'/'
+    id_str = date + '_' + tag + '/'
+    output_dir += id_str
+    print( output_dir)
+    os.mkdir( output_dir)
         
-        #Speichern der Datei 
-        filename = output_dir + secure_filename(f.filename)
-        f.save(filename) 
+    #Speichern der Datei 
+    #filename = output_dir + secure_filename(f.filename)
+    filename = output_dir + "protein.pdb" 
+    f.save(filename) 
         
-        #call main executable
-        #process = Popen(["wine", "exec/get_volume.exe ex:0.1 rad:protor i:" + filename + " o:" + filename[:-4] + ".vol"], stdout=PIPE, stderr=PIPE)
-        #process = Popen(["python", "exec/test.py"], stdout=PIPE,stderr=PIPE)
-        
-        #cmd = "wine exec/get_volume.exe ex:0.1 rad:protor i:" + filename + " o:" + filename[:-4] + ".vol"
-        
-        #print(stdout)
-#        print(cmd)
-#        os.system(cmd)
-        
-        #call voronoia.py
-#        cmd = "voronoia.py " + filename + " -o " + filename[:-4] + ".tmp -vd"
-#        print(cmd)
-#        os.system(cmd)
-        
-        
-        #return redirect('/status') # uebergabe pfad an ausgabe seite
-        print("test")
-        return {'Location': url_for('status'), 'tag':tag}
-        #return redirect('methods', code=278)
-        #return render_template('formular.html', form=form, tag_str="tag")
-        #return redirect(url_for('methods'))
-        #return redirect('methods', code=278)
-        #return redirect('methods', code=302)
-        #return redirect(url_for('methods'))
-        #print(url_for('methods'))
-        #return redirect(url_for('methods'))
-        #return render_template( "formular.html",form=form ,tag_str=tag )
-        #print("check")
-    return render_template('formular.html', form=form, tag_str="tag")
+    #call main executable
+    #process = Popen(["wine", "exec/get_volume.exe", "ex:0.1", "rad:protor" ,"i:" + filename + " o:" + filename[:-4] + ".vol"], stdout=PIPE, stderr=PIPE)
+    #process = Popen(["python", "exec/test.py"], stdout=PIPE,stderr=PIPE)
+    #cmd = "wine /home/hildilab/app/v4rna/exec/get_volume.exe ex:0.1 rad:protor i:%(data_pdb)s o:%(outputdir)s/%(BASE)s.vol >& %(BASE)s.log" % {"data_pdb": filename, "outputdir": "", "BASE": filename[:-4]}
+     
+    #call voronoia.py
+    cmd = ["voronoia.py",filename , "-o", filename[:-4] + "_out","-vd"]
+    print(cmd)
+    # os.system(cmd)
+    p = subprocess.check_output(cmd) #,stdout=subprocess.PIPE)
+    print( p)
+    # thread.start_new_thread( os.system , ( 'gedit ', ))
+    print( "command terminated")
+    
 
-@app.route('/status')
-def status():
-    return render_template('status.html')
 
-@app.route('/results')
-def result():
-    tag = request.url.split('=')[-1]
-    con = sql.connect(sql_db)
-    con.row_factory = sql.Row
-    cur = con.cursor()
-    cur.execute("SELECT pdb FROM jobs WHERE tag = '" + tag + "'")
-    rows = cur.fetchall()
-    pdb = rows[0]['pdb']
-    con.close()
-    return render_template('results.html', pdb=pdb, tag=tag)
+@app.route('/status/<user>/<job>')
+def status( user, job):
+    files_all_exist = True
+    status = 'running'
+    # check whether output files exist:
+    # rather use walk, dirname is not job but date_job
+    for name in ["protein.vol","protein.vol.extended.vol"]:
+        fname = os.path.join( app.config['USER_DATA_DIR'] , user, job, name)
+        if not os.path.isfile(fname):
+            files_all_exist = False
+            break
+    if files_all_exist:
+        status = 'finished'
+    
+    return jsonify( {'status':status} )  # returns dict 
+
+
+
+@app.route('/downloads/<user>/<job>/<filename>')
+def download( user, job, filename):
+    path = app.config['USER_DATA_DIR'] + '/' + user + '/' + job
+    print( "download:", path)
+    return send_from_directory( path,filename)
+
+
+
+@app.route('/results/<user>/<tag>')
+def result(user, tag):
+    # check first if the tag is in the db:
+    #tag = request.url.split('=')[-1]
+    #con = sql.connect(sql_db)
+    #con.row_factory = sql.Row
+    #cur = con.cursor()
+    #cur.execute("SELECT pdb FROM jobs WHERE tag = '" + tag + "'")
+    #rows = cur.fetchall()
+    #pdb = rows[0]['pdb']
+    #con.close()
+    # if not, check the user data:
+
+    pdb_dir = ""
+    for main, dirs, files in os.walk( os.path.join( app.config['USER_DATA_DIR'] , user )):
+        for mdir in dirs:
+            if tag in mdir:
+                pdb_dir = mdir
+                break
+        if pdb_dir != "":
+            break
+    status_url = "/status/" + user + '/' + tag
+    #dir = os.system("find ./ -name '*" + tag + "'") + "/"
+    #pdb = dir + os.system("ls " + dir + " | grep .pdb")
+    print("pdb name incoming")
+    print(pdb_dir)
+    return render_template('results.html', user=user, job=pdb_dir, status_url=status_url)
+
+
 
 @app.route('/databank')
 def databank():
+    print('i am alive and well')
+    print(sql_db)
     con = sql.connect(sql_db)
     con.row_factory = sql.Row
     cur = con.cursor()
-    ranStr = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(4))
-    ranTag = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(2))
-    cur.execute("INSERT INTO jobs (id,tag,pdb,vol) VALUES (?,?,?,?)" , ("1",ranTag,ranStr + ".pdb",ranStr + ".vol") )
     cur.execute("SELECT * FROM jobs")
     con.commit()
     rows = cur.fetchall()
     con.close()
     return render_template('databank.html', rows = rows)
 
+
+
 @app.route('/methods')
 def methods():
     return render_template('methods.html')
+
+
 
 @app.route('/references')
 def references():
